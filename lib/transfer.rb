e1265f11254e332b428e74f4c44b4ea5b3a46afa
Parker Williamson <parker.williamson@gmail.com>
2020-05-19 19:45:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,6 +3,9 @@ class Transfer
   attr_accessor :sender, :receiver, :amount, :status
   
   def initialize(sender, receiver, amount)
-    @transfer = transfer
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount 
+    @status = "pending"
   end
 end
