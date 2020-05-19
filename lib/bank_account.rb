1fe51f868811f86810d39c0008cf6750c4dd5ee2
Parker Williamson <parker.williamson@gmail.com>
2020-05-19 19:41:05 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,5 +16,12 @@ class BankAccount
   def display_balance
     "Your balance is $#{@balance}."
   end
-
+  
+  def valid?
+    @status == "open" && @balance > 0 ? true : false 
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 end
