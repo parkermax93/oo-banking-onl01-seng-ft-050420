2266ba056eb69762219818b681c28a38ae5b07bf
Parker Williamson <parker.williamson@gmail.com>
2020-05-19 21:06:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,12 +23,4 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
-  
-  def reverse_transfer
-    if @status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
-      @status = "reversed"
-    end
-  end
 end
